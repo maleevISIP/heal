8988b6512d3209963bc96e775575a353a68b9439
Egor Maleev <egor.maleev@list.ru>
2020-06-18 23:00:22 +0500

    egorka

---
+++ Health/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health
+{
+    class Heal
+    {
+        private double temperatur;
+        public double temperaturZnach
+        {
+            get
+            {
+                return temperatur;
+            }
+            set
+            {
+                if (value < 34)
+                    temperatur = 34;
+                if (value > 41)
+                    temperatur = 41;
+                else temperatur = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heal Health = new Heal();
+            Health.temperaturZnach = 100;
+            Console.WriteLine(Health.temperaturZnach + " (Стандарт -- 41 град.)");
+            Health.temperaturZnach = 36.6;
+            Console.WriteLine(Health.temperaturZnach);
+            Console.ReadKey();
+        }
+    }
+}
